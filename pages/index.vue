23602e4af9afb38bcf763dd6a429228eab911097
Faishal Arif <faishal.arif@metraplasa.co.id>
2019-04-23 15:41:26 +0700

    Add function and logical for check input in every element array nominal rupiahs

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,13 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        minimum-number-of-rupiahs
-      </h1>
-      <h2 class="subtitle">
-        Faishal&#39;s project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
+  <div class="index-container">
+    <input type="text" style="font-size: 40px;" v-model="inputAmmount" @keydown.enter="checkAmmount">
+    <div class="index-content">
+      <button class="index-content__btn" @click="checkAmmount">Check</button>
     </div>
-  </section>
+    <div class="index-content" v-for="item in result" :key="item.nominal">
+      <h5>{{ item.jumlah }}x Rp {{ item.nominal }}</h5>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -30,18 +16,54 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo
+  },
+  data() {
+    return {
+      inputAmmount: '',
+      dataArray: [100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 100, 50 ],
+      result: ''
+    }
+  },
+  methods: {
+     checkAmmount() {
+      let tempAmmount = this.inputAmmount
+      if (this.inputAmmount !== '') {
+        this.arrayAmount = []
+        this.inputAmmount = JSON.parse(this.inputAmmount)
+        this.result = this.dataArray.map(e => {
+          if (tempAmmount >= e) {
+            let jumlah = tempAmmount / e
+            tempAmmount = tempAmmount % e
+            return {'nominal': e,'jumlah' : Math.floor(jumlah)}
+          }
+        }).filter(function( element ) {
+          return element !== undefined
+        })
+      }
+    }
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<style lang="scss" scoped>
+.index {
+  &-container {
+    margin: 0 auto;
+    min-height: 100vh;
+    margin-top: 30%;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+  &-content {
+    margin-top: 20px;
+    &__btn {
+      background-color: dodgerblue;
+      color: white;
+      border: 0px;
+      padding: 7px 10px 7px 10px;
+    }
+  }
 }
 
 .title {
